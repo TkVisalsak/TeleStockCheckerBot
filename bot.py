e566ea22b4233bfbefef040abcdee7d5e843eff0
TkVisalsak <visalsak2005@gmail.com>
2025-01-16 04:10:50 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -186,7 +186,13 @@ def main():
     application.add_handler(CommandHandler("check", check_items))
     application.add_handler(CommandHandler("search", search_item))
 
-    application.run_polling()
+    application.run_webhook(
+    listen="0.0.0.0",
+    port=int(os.getenv("PORT", 8443)),
+    url_path="webhook",
+    webhook_url=f"https://{os.getenv('RENDER_EXTERNAL_URL')}/webhook"
+)
+
 
 if __name__ == '__main__':
     main()
